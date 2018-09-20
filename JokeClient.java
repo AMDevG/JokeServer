f03b460d361844bd9fe48e53bd907ecff7eac834
AMDevG <jeberry308@gmail.com>
2018-09-19 21:58:04 -0500

    Sending Test Joke on Connection

---
+++ JokeClient.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.net.*;
+
+public class JokeClient {
+    
+    public static void main (String args[]){
+
+        String serverName;
+        if(args.length < 1) serverName = "localhost";
+        else serverName = args[0];
+        System.out.println("John Berry's Inet Client, 1.8.\n");
+        System.out.println("Using server: " + serverName + ", Port: 3024");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        try{
+          
+            do{
+                // System.out.print
+                //         ("Enter a hostname or an IP address, (quit) to end: ");
+                // System.out.flush();
+                //name = in.readLine();
+                //if(name.indexOf("quit") < 0)
+                    getRemoteAddress(serverName);
+
+            } while (true);
+            
+        } catch (Exception x) {x.printStackTrace();}
+    }
+    
+
+    static String toText (byte ip[]){
+        StringBuffer result = new StringBuffer();
+        for(int i = 0; i < ip.length; ++i){
+            if (i > 0) result.append(".");
+            result.append(0xff & ip[i]);
+        }
+        return result.toString();
+    }
+    
+
+
+static void getRemoteAddress (String serverName){
+
+	/* getRemoteAddress does most of the heavy lifting in communicating with the server.
+	It first creates a new socket (sock), passing the name of the server (localhost) and specifying which port
+	to connect to (1565) */
+
+    Socket sock;
+    BufferedReader fromServer;
+    PrintStream toServer;
+    String textFromServer;
+    
+    try{
+        sock = new Socket(serverName, 3024);
+
+    /* An attempt to connect to the server is executed when the socket, named sock here, is created.
+    	Variables fromServer, toServer, and textFromServer are created to read and write output between
+    	client and server */
+
+        fromServer =
+                new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        //toServer = new PrintStream(sock.getOutputStream());
+
+        //toServer.println(name); toServer.flush();
+
+        for (int i = 1; i<=3; i++){
+            textFromServer = fromServer.readLine();
+            if (textFromServer != null) System.out.println(textFromServer);
+        }
+
+        /* readLine() is called on fromServer which retrieves outputfrom the server.
+    	If it is not null, the text will be printed to the client console. (i.e. if the server
+    	returns an IP address or an exception, the user will see the output.) and finally the
+    	socket is closed */
+
+        sock.close();
+    } catch (IOException x){
+        System.out.println("Socket error");
+        x.printStackTrace();
+    }
+  }  
+}
---
+++ JokeServer.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+class Worker extends Thread{
+    Socket sock;
+    Worker (Socket s) {sock = s;}
+
+/* Worker class extends the Thread class making it a subclass so that it can make use of Java's
+  threading capabilities. Threading is necessary to the server's functionality so that the server can 
+  accept multiple client requests simultaneously (or almost simultaneously)*/
+
+public void run(){
+
+    PrintStream out = null;
+    BufferedReader in = null;
+    
+    try{
+        in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        out = new PrintStream(sock.getOutputStream());
+    try{
+
+      /* run() function creates variables 'in' and 'out' to retrieve client input. A variable
+        of type String 'name' is created and reads in user input */
+
+        /*String name;
+        name = in.readLine();
+        System.out.println("Looking up: " + name);*/
+        printRemoteAddress(out);
+
+        /* printRemoteAddress is called from within run function, catching any IO exceptions,
+        and finally closing the socket */
+
+    } catch (Exception x){
+        System.out.println("Server read error"); 
+    }
+    
+    }catch(Exception e){System.out.println("Error");
+    }
+}
+
+static void printRemoteAddress (PrintStream out){
+
+    try{
+        out.println("Why did chicken cross road");
+    }catch(Exception ex){
+        out.println("Failed in attempt to look  up ");
+    }
+    
+}
+
+static String toText (byte ip[]) {
+  /* toText is used to make the output of getAddress() human readable */
+ StringBuffer result = new StringBuffer ();
+ for (int i = 0; i < ip.length; ++ i) {
+ if (i > 0) result.append (".");
+ result.append (0xff & ip[i]);
+ }
+ return result.toString ();
+ }
+}
+
+public class JokeServer {
+
+     public static ArrayList<String> jokeList;
+
+    
+    /* InetServer's main function creates a new ServerSocket that will accept client connections.
+      The server will accept new connections until it is terminated due to the while(true) condition
+      which will always remain true. When a new connection is accepted, a new Worker thread is created
+      to handle the client requests. The server listens on port 3024 for any incoming connections. */
+
+    public static void main(String[] args) throws IOException {
+       int q_len = 6;
+       int port = 3024;
+       Socket sock;
+       ServerSocket servsock = new ServerSocket(port, q_len);
+
+       
+       System.out.println("John Berry's Inet Server 1.8 starting up, listening at port 3024. \n");
+       while(true){
+           sock = servsock.accept();
+           new Worker(sock).start();
+       }
+    }
+}
